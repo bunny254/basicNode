c91b9f8621c5731777b46ef6f23d8b9837a88232
unknown <kimaniwachira03@gmail.com>
2022-02-28 15:53:45 +0300

    Node Modules System

--- logger.js
+++ logger.js
@@ -1,10 +1,24 @@
 console.log(__filename);
 console.log(__dirname);
 
+const EventEmitter=require('events')
+const emitter=new EventEmitter();
+
 const url = "http://localhost:3000";
 
-function log(message) {
+class Logger extends EventEmitter{
+    log(message){
+        //Send HTTP request
+        console.log(message);
+        //Raise an event
+        this.emit('messageLogged',{id:1, url:'http://'});
+    }
+}
+
+module.exports=Logger;
+/*function log(message) {
   //send Http request
   console.log(message);
 }
 module.exports = log;
+*/
